3ec1db58183478770c8f174edae3492dd2ab27ff
Lily Misra <bigpumpkin236@outlook.com>
2020-12-24 10:23:11 -0600

    Add JS functionality for images

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-const currentImageAttrs = {};
+const displayImage = document.querySelector("#display img");
+const fsImages = document.querySelectorAll("#film-strip img");
 
-const display = document.querySelector("#display img");
-const fsImages = document.querySelectorAll("film-strip img");
+fsImages.forEach((fsImage) => {
+  fsImage.addEventListener("click", (event) => {
+    displayImage.src = event.target.src;
+    displayImage.alt = event.target.alt;
+  });
+});
